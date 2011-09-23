bad4f0daa3b2ab144f70eb2d2620abaf4313133f
Yaffle <vic99999@yandex.ru>
2011-09-23 10:01:26 +0600

    example in PHP

---
+++ php/events.php
@@ -0,0 +1,31 @@
+<?
+
+      header('Content-Type: text/event-stream');
+      header('Access-Control-Allow-Origin: *');
+      header('Cache-Control: no-cache');
+
+      @apache_setenv('no-gzip', 1);
+      @ini_set('zlib.output_compression', 0);
+      @ini_set('implicit_flush', 1);
+      for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
+      ob_implicit_flush(1);
+
+      if (preg_match('#Last\\-Event\\-ID\\=([\\s\\S]+)#ui', @$HTTP_RAW_POST_DATA, $tmp)) {
+        $lastEventId = urldecode(@$tmp[1]);
+      } else {
+        $headers = getallheaders();
+        $lastEventId = @$headers['Last-Event-ID'];    
+      }
+
+      echo ':' . str_repeat(' ', 2048) . "\n"; // 2kb padding for IE
+
+      for ($i = intval($lastEventId) + 1; $i < 100; $i++) {
+        echo "id: $i\n";
+        if (mt_rand(0, 20) < 5) {
+          exit(); // drop connection
+        }
+        echo "data: $i;\n\n";
+        sleep(1);
+      }
+
+?>
