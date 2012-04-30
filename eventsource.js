88074f1dc11d067adcefa922ca29c7e12bbf64e5
Yaffle <vic99999@yandex.ru>
2012-04-30 10:36:54 +0600

    fix

--- eventsource.js
+++ eventsource.js
@@ -238,6 +238,7 @@
         if (contentType && (/^text\/event\-stream/i).test(contentType)) {
           queue({type: 'open'}, OPEN);
           opened = true;
+          retry2 = retry;
         }
       }
 
