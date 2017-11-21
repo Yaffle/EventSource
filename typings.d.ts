182508f1727e5808196200ada904940a4bea97fa
steven.guiheux <steven.guiheux@corp.ovh.com>
2017-11-21 17:18:33 +0100

    feat: force reconnect even when there is activity

--- typings.d.ts
+++ typings.d.ts
@@ -1,25 +1,28 @@
 declare enum ReadyState {CONNECTING = 0, OPEN = 1, CLOSED = 2}
 
 export class EventSourcePolyfill extends EventTarget {
-  CONNECTING: ReadyState;
-  OPEN: ReadyState;
-  CLOSED: ReadyState;
+    CONNECTING: ReadyState;
+    OPEN: ReadyState;
+    CLOSED: ReadyState;
 
-  constructor(url: string, eventSourceInitDict?: EventSourceInit);
-  url: string;
-  readyState: ReadyState;
-  onopen: Function;
-  onmessage: (event: OnMessageEvent) => void;
-  onerror: Function;
-  close: () => void;
+    constructor(url: string, eventSourceInitDict?: EventSourceInit);
+
+    url: string;
+    readyState: ReadyState;
+    onopen: Function;
+    onmessage: (event: OnMessageEvent) => void;
+    onerror: Function;
+    close: () => void;
 }
 
 interface EventSourceInit {
-  withCredentials?: boolean;
-  headers?: Object;
-  errorOnTimeout?: boolean;
+    withCredentials?: boolean;
+    headers?: Object;
+    errorOnTimeout?: boolean;
+    heartbeatTimeout?: number;
+    checkActivity?: boolean;
 }
 
 interface OnMessageEvent {
-  data: string;
+    data: string;
 }
